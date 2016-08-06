fa857d0caacb4f12801c44164e969474a5e7c8fb
Paul Gowder <paul.gowder@gmail.com>
2016-08-06 16:01:59 -0400

    minimal working with webpack ./main.js bundle.js

---
+++ funcs.js
@@ -0,0 +1,9 @@
+function kitty() {
+	document.write("KITTY!");
+}
+
+function puppy() {
+	document.write("PUPPY!");
+}
+
+module.exports = {"puppy": puppy, "kitty": kitty}
---
+++ main.js
@@ -0,0 +1,5 @@
+var funcs = require("./funcs.js");
+console.log(funcs);
+
+funcs.kitty()
+funcs.puppy()
